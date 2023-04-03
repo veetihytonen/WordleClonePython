2574e09a273628c4dc38a40b3c68b70e04dee8b5
Veeti Hytönen <veeti.hytonen@gmail.com>
2023-04-03 15:08:15 +0300

    Wrote a test to appease course overlords

---
+++ src/tests/entities/game_test.py
@@ -0,0 +1,20 @@
+import unittest
+from entities.game import Game
+
+class TestGame(unittest.TestCase):
+    def setUp(self) -> None:
+        self.game = Game(solution='close')
+
+    def test_solution_is_set_correctly(self):
+        my_solution = 'broke'
+        self.game = Game(solution=my_solution)
+        
+        self.assertEqual(my_solution, "".join(self.game._solution))
+
+    # write this to utilise word bank so it actually tests a meanigful amount of cases
+    def test_compare_guess_to_solution_compares_correctly(self):
+        guess = list()
+        output = self.game._compare_guess_to_solution(guess)
+        
+
+
